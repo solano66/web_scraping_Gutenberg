ef5d1aac623d6af83e4f66c4f3310fc63eb8a33e
solano66 <solano66279431@gmail.com>
2024-12-15 15:06:30 +0800

    update readme & cpmment

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# web_scraping_Gutenberg
-web scraping practice - Project Gutenberg
+# Project Gutenberg Scraper
+
+A Python script to scrape Chinese books from [Project Gutenberg](https://www.gutenberg.org), an extensive online library of free eBooks. This project demonstrates basic web scraping techniques using the `requests` and `BeautifulSoup` libraries.
+
+## Project Structure
+```
+.
+├── gutenberg.py
+├── project_gutenberg/
+│   └── [scraped text files]
+├── requirements.txt
+```
+
+## Installing
+   ```bash
+   pip install -r requirements.txt
+   ```
+- `requests==2.32.3` - For sending HTTP requests to the website.
+- `BeautifulSoup==4.12.3` - For parsing and navigating HTML content.
+
+## Coding style
+Follow PEP8 unless explicitly specified otherwise
+
+## Deployment
+   ```bash
+   python gutenberg.py
+   ```
+The script will start scraping Chinese books from Project Gutenberg and save the results as a `.txt` file in the `project_gutenberg` folder.
+
+## Demo
+<iframe width="720" height="405" src="https://www.youtube.com/embed/_mD591UNjw8" frameborder="0" allowfullscreen></iframe>
+
+## Versioning
+- Python 3.10
+
+## Authors
+[solano66](https://github.com/solano66)
+
+## Acknowledgments
+Thank you to all the people who release code on GitHub.
--- gutenberg.py
+++ gutenberg.py
@@ -1,33 +1,41 @@
+import os
+import re
 import requests
 from bs4 import BeautifulSoup as bs
-import re
-import os
 
-folderPath = 'project_gutenberg'
-if not os.path.exists(folderPath):
-    os.mkdir(folderPath)
+# 檢查儲存資料的資料夾是否存在
+folder_path = 'project_gutenberg'
+if not os.path.exists(folder_path):
+    os.mkdir(folder_path)
 
+# 設定目標網址
 url = 'https://www.gutenberg.org/browse/languages/zh'
-res = requests.get(url)
-soup = bs(res.text, "lxml")
+response = requests.get(url)
+soup = bs(response.text, "lxml")
 
+# 初始化變數
 list_posts = []
-list_posts.clear()
 count = 1
 
+# 選取中文書籍的連結
 chinese_books = soup.select('div.pgdbbylanguage > ul > li.pgdbetext > a')
 
 regex_no = r'/ebooks/(\d+)'
 regex_chinese = r'^[\u4E00-\u9FFF]'
 
 for book in chinese_books:
+    # 檢查是否為純中文書
     match_chinese = re.match(regex_chinese, book.get_text())
 
     if not match_chinese:
         continue
-
-    book_name = re.sub(r'[\s<>:"/\\|?*]', '',book.get_text())
+    
+    # 去除無法命名的符號
+    book_name = re.sub(r'[\s<>:"/\\|?*]', '', book.get_text())
+    # 取得書本編號
     match_no = re.match(regex_no, book['href'])
+
+    # 建立 list
     list_posts.append({
         'order': count,
         'ID': match_no.group(1),
@@ -36,23 +44,21 @@ for book in chinese_books:
     })
     count += 1
 
-for index, obj in enumerate(list_posts):
+# 下載每本書的內容
+for obj in list_posts:
     res_book = requests.get(obj['link'])
     soup_book = bs(res_book.text, "lxml")
 
-    for garbage in soup_book.select('head'):
-        garbage.decompose()
-    for garbage in soup_book.select('section.pg-boilerplate.pgheader'):
-        garbage.decompose()
-    for garbage in soup_book.select('p#id00000'):
+    # 移除多餘的 HTML 元素
+    for garbage in soup_book.select('head, section.pg-boilerplate.pgheader, p#id00000'):
         garbage.decompose()
 
+    # 提取純文本
     txt = soup_book.get_text()
 
-    with open(f'{folderPath}/{obj["order"]}_{obj["title"]}_{obj["ID"]}.txt', 'w', encoding='utf-8') as file:
+    # 儲存為 .txt 檔案
+    file_name = f'{folder_path}/{obj["order"]}_{obj["title"]}_{obj["ID"]}.txt'
+    with open(file_name, 'w', encoding='utf-8') as file:
         file.write(txt)
-    
-    print(f'已下載 {obj["order"]}_{obj["title"]}_{obj["ID"]}')
 
-    
-    
+    print(f'已下載 {obj["order"]}_{obj["title"]}_{obj["ID"]}')
